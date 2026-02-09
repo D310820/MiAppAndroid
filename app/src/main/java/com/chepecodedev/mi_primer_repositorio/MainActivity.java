c5203e10135ef15d2190f3555d5326e9c9826037
Josè Daniel Espinoza Gomez <reparacionesjd17@gmail.com>
2026-02-08 23:53:57 -0600

    carga inicial

--- app/src/main/java/com/chepecodedev/mi_primer_repositorio/MainActivity.java
+++ app/src/main/java/com/chepecodedev/mi_primer_repositorio/MainActivity.java
@@ -1,24 +1,39 @@
 package com.chepecodedev.mi_primer_repositorio;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Enlaza el botón
+        Button btnGitHub = findViewById(R.id.btnGitHub);
+
+        // Configura el clic
+        btnGitHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String githubUrl = "https://github.com/D310820/MiAppAndroid.git";
+                // Crea el intent para abrir el navegador
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl));
+
+                // Verifica que haya una app que pueda manejar el intent (navegador)
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    // Mensaje si no hay navegador instalado
+                    Toast.makeText(MainActivity.this,
+                            "No se encontró un navegador para abrir el enlace",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
